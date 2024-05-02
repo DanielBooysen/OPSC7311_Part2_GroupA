f2956f930f0e63465fe5fbda3d1cf2646f1d6695
Jacques Durandt <st10242724@vcconnect.edu.za>
2024-05-02 16:59:55 +0200

    Daily Goals

--- app/src/main/java/com/example/opsc7311_part2_groupa/Goal.kt
+++ app/src/main/java/com/example/opsc7311_part2_groupa/Goal.kt
@@ -3,11 +3,14 @@ package com.example.opsc7311_part2_groupa
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 
 class Goal : AppCompatActivity() {
+
+    private val goals = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_goal)
