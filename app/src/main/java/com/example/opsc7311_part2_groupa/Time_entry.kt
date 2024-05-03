e6d3749f0639e1913457e896e634c0c2d6b2ac73
ST10145185 <ST10145185@vcconnect.edu.za>
2024-05-03 12:30:31 +0200

    Success prompt time entry

--- app/src/main/java/com/example/opsc7311_part2_groupa/Time_entry.kt
+++ app/src/main/java/com/example/opsc7311_part2_groupa/Time_entry.kt
@@ -14,6 +14,7 @@ import android.widget.ListView
 import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
@@ -162,7 +163,15 @@ class Time_entry : AppCompatActivity() {
             data.put("category", categoryChosen)
             data.put("description", description)
             data.put("date", date)
-            db.insert("entries", null, data)
+            val insert: Long = db.insert("entries", null, data)
+
+            if(!insert.equals(-1)){
+                val ad = AlertDialog.Builder(this)
+                ad.setTitle("Message")
+                ad.setMessage("Success")
+                ad.setPositiveButton("Ok", null)
+                ad.show()
+            }
 
             setContentView(R.layout.item_timesheet_entry)
 
